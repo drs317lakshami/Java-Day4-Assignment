653782f5f583adb78971ce1ffe306924b1a9b872
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-30 14:53:14 +0530

    Welcome To The Snake And Ladder Program

---
+++ SnakeAndLadderSimulator.java
@@ -0,0 +1,11 @@
+package com.blz.shubham.employeewages;
+
+
+public class SnakeAndLaddersProgram {
+
+    public static void main(String [] args){
+
+        System.out.println("Welcome To Snake And Ladder Simulator");
+    }
+
+}
